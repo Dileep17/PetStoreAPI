f7888a4ff55d21141d1d1921651432d02b748285
Dileep B <dileep.srimani@gmail.com>
2018-06-10 11:01:04 +0530

    [Dileep] Add first endpoint

---
+++ PetStoreAPI/Controllers/PetsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PetStoreAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        private PetContext _context;
+
+        public PetsController(PetContext context)
+        {
+            _context = context;
+            if (_context.Pets.Count() == 0)
+            {
+                _context.Pets.Add(new Pet { Name = "Cat", Owner = "CatWomen", Family = "CatWomen" });
+                _context.SaveChanges();
+            }
+
+        }
+
+
+
+        [HttpGet]
+        public ActionResult<List<Pet>> GetAllPets()
+        {
+            return _context.Pets.ToList();
+
+        }
+    }
+}
---
+++ PetStoreAPI/Models/Pet.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace PetStoreAPI.Models
+{
+    public class Pet
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { set; get; }
+        public string Family { set; get; }
+        public string Owner { set; get; }
+
+    }
+}
